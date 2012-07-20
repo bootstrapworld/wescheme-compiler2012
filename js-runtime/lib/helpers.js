1222001c1efe8b42692d8e26845761408f48ea5c
Andrew Tian <andrew_tian@brown.edu>
2012-07-20 14:35:59 -0400

    fixed position stack not defined in check

--- js-runtime/lib/helpers.js
+++ js-runtime/lib/helpers.js
@@ -251,6 +251,12 @@ var helpers = {};
 	};
 
 	var throwCheckError = function(aState, details, pos, args) {
+
+		var positionStack = 
+        		state.captureCurrentContinuationMarks(aState).ref(
+            		types.symbol('moby-application-position-key'));
+        
+
 		if(aState === undefined || (positionStack[positionStack.length - 1] === undefined)) {
 			throwUncoloredCheckError(aState, details, pos, args);
 		}
--- servlet-htdocs/support.js
+++ servlet-htdocs/support.js
@@ -835,6 +835,12 @@ var helpers = {};
 	};
 
 	var throwCheckError = function(aState, details, pos, args) {
+
+		var positionStack = 
+        		state.captureCurrentContinuationMarks(aState).ref(
+            		types.symbol('moby-application-position-key'));
+        
+
 		if(aState === undefined || (positionStack[positionStack.length - 1] === undefined)) {
 			throwUncoloredCheckError(aState, details, pos, args);
 		}
