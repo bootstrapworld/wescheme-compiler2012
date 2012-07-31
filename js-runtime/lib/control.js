11691953122aa153d3e85597cdc1d58a803195fb
Mike Rowland <mjrowlan@cs.brown.edu>
2012-07-31 10:02:34 -0400

    fixed spacing bug

--- js-runtime/lib/control.js
+++ js-runtime/lib/control.js
@@ -843,15 +843,15 @@ var callContinuationProcedure = function(state, procValue, n, operandValues) {
 // selectProcedureByArity: state (CaseLambdaValue | CasePrimitive | Continuation | Closure | Primitive) -> (Continuation | Closure | Primitive)
 var selectProcedureByArity = function(aState, n, procValue, operands) {
     var getArgStr = function() {
-	var argStr = '';
-	if (operands.length > 0) {
-		var argStrBuffer = [':'];
-		for (var i = 0; i < operands.length; i++) {
-			argStrBuffer.push( types.toWrittenString(operands[i]) );
-		}
-		argStr = argStrBuffer.join(' ');
-	}
-	return argStr;
+    	var argStr = '';
+    	if (operands.length > 0) {
+    		var argStrBuffer = [':'];
+    		for (var i = 0; i < operands.length; i++) {
+    			argStrBuffer.push( types.toWrittenString(operands[i]) );
+    		}
+    		argStr = argStrBuffer.join(' ');
+    	}
+    	return argStr;
     }
     
     var getArgColoredParts = function(locations) {
@@ -861,7 +861,7 @@ var selectProcedureByArity = function(aState, n, procValue, operands) {
     	if (operands.length > 0) {
     		for (var i = 0; i < operands.length; i++) {
                 space = (locs.rest().isEmpty() ? "" : " ");
-    			argColoredParts.push(new types.ColoredPart(operands[i]+" ", locs.first()));
+    			argColoredParts.push(new types.ColoredPart(operands[i]+space, locs.first()));
     			locs = locs.rest();
     		}
     	}
@@ -889,8 +889,6 @@ var selectProcedureByArity = function(aState, n, procValue, operands) {
             new types.Message([new types.MultiPart("function call", [op, cp]),
                                 ": expected function, given: ",
                                 new types.ColoredPart(procValue, locationList.first())
-                                //((operands.length == 0) ? ' (no arguments)' : '; arguments were '),
-                               // ((operands.length != 0) ? new types.GradientPart(argColoredParts) : ''),
                                 ]),
                              []));
 
@@ -928,7 +926,8 @@ var selectProcedureByArity = function(aState, n, procValue, operands) {
                            n,
                            new types.GradientPart(argColoredParts)]),	
 		[]));
-    } else if (procValue instanceof primitive.CasePrimitive) {
+    } 
+    else if (procValue instanceof primitive.CasePrimitive) {
 	for (var j = 0; j < procValue.cases.length; j++) {
 	    if (n === procValue.cases[j].numParams ||
 		(n > procValue.cases[j].numParams && 
