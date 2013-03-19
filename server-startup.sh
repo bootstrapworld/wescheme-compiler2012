4e3c0ae0416b3c17b45eaaefa385a7f49331a64c
Danny Yoo <dyoo@hashcollision.org>
2013-03-19 13:10:33 -0600

    Adding calls to keep the software up to date.

--- server-startup.sh
+++ server-startup.sh
@@ -2,6 +2,7 @@
 
 ## This is the startup script for the EC2 server instances.
 
-cd /home/ubuntu/wescheme-compiler2012
+git fetch
+git merge stable
 /home/ubuntu/racket/bin/raco make compiler-service.rkt
 /usr/bin/nohup /home/ubuntu/racket/bin/racket compiler-service.rkt  --extra-module-provider wescheme-module-provider.rkt &
