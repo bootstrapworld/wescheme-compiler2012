80beb3ff7882bd6a1b3e84c4782cb4920a9e741b
Danny Yoo <dyoo@hashcollision.org>
2013-04-13 18:33:10 -0600

    updating the startup scripts

--- server-startup.sh
+++ server-startup.sh
@@ -3,6 +3,6 @@
 ## This is the startup script for the EC2 server instances.
 
 git fetch
-git merge origin/stable
+git reset --hard origin/stable
 /home/ubuntu/racket/bin/raco make compiler-service.rkt
 /usr/bin/nohup /home/ubuntu/racket/bin/racket compiler-service.rkt  --extra-module-provider wescheme-module-provider.rkt &
