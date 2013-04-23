046ae7eaab1ff7951dc0abfc74799c4f7276b280
Danny Yoo <dyoo@hashcollision.org>
2013-04-23 17:26:43 -0600

    adding those arguments to the sandboxed server

--- server-startup.sh
+++ server-startup.sh
@@ -6,4 +6,4 @@ git fetch
 git checkout stable
 git merge origin/stable
 /home/ubuntu/racket/bin/raco make compiler-service.rkt
-/usr/bin/nohup /home/ubuntu/racket/bin/racket compiler-service.rkt  --extra-module-provider wescheme-module-provider.rkt &
+/usr/bin/nohup /home/ubuntu/racket/bin/racket sandboxed-server.rkt --port 8000 --extra-module-provider wescheme-module-provider.rkt &
